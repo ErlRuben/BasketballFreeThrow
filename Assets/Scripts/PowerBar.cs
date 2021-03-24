d27a2874dc840a7500dc1bfa7934a9defdffa6c8
Earl Batenga <61070797+Elchupappi@users.noreply.github.com>
2021-03-24 08:39:01 +0800

    s

---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PowerBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image imagePower;
+    [SerializeField]
+    private TMP_Text textPower;
+
+    private bool isPowerUp = false;
+    private bool isDirectionUp = true;
+    private float ammntPower = 0.0f;
+    private float speedPower = 500.0f;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPowerUp){
+            PowerActive();
+        }
+    }
+    public void PowerActive(){
+        if(isDirectionUp){
+            ammntPower += Time.deltaTime * speedPower;
+            if(ammntPower > 100){
+                isDirectionUp = false;
+                ammntPower = 100.0f;
+            }
+        }
+        else{
+            ammntPower -= Time.deltaTime * speedPower;
+            if (ammntPower < 0){
+                isDirectionUp = true;
+                ammntPower = 0.0f;
+            }
+        }
+        imagePower.fillAmount = (0f - 1f) * ammntPower / 100.0f + 1f;
+    }
+    public void PowerUp()
+    {
+        isPowerUp = true;
+        ammntPower = 0.0f;
+        isDirectionUp = true;
+        textPower.text = ammntPower.ToString("F0");
+    }
+    public void EndPower()
+    {
+        isPowerUp = false;
+        textPower.text = ammntPower.ToString("F0");
+    }
+}
